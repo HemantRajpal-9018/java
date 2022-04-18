f2c52cd730a40a04af0959f7307b6b442b1f78d5
HemantRajpal2021 <hemant.rajpal2021@gmail.com>
2022-04-18 00:41:25 -0400

    Uploaded java file

---
+++ Hello.java
@@ -0,0 +1,21 @@
+
+
+/**
+ * @author hemant
+ *
+ */
+public class Hello {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		int x = 20, y = 50, sum;  
+		sum = x + y;  
+		System.out.println("The sum of numbers is: "+sum);  
+		}  
+		}  
+
+
+
+
